7ec1c390f2138399fc75c4ac8e90e6931fad9feb
Diwansh Giri <diwanshgiri@gmail.com>
2026-08-13 05:27:39 +0300

    Add Google Analytics tag

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css";
 
+const googleTagId = "G-F877GL8V0N";
+
 export const metadata = {
   title: "Halesi Ravintola",
   description: "Nepalese restaurant in the heart of Hyvinkaa with online reservations."
@@ -8,6 +10,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src={"https://www.googletagmanager.com/gtag/js?id=" + googleTagId} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag("js", new Date());
+              gtag("config", "G-F877GL8V0N");
+            `
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
